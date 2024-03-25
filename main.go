15992b603fdb4df812f0bbe418267f18b01d541e
yuanhongbo <yhb3420@qq.com>
2024-03-26 00:52:38 +0800

    init

--- main.go
+++ main.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"log"
-	"time"
+	"os"
+
+	dmodel "dingtou/model"
+
+	"github.com/joho/godotenv"
+
+	"encoding/json"
 )
 
 func main() {
-	log.Printf("start dingtou app. now:%s", time.Now())
+	log.Printf("start dingtou app.")
+
+	// 从本地读取环境变量
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// 数据库初始化
+	dsn := os.Getenv("DB_DSN")
+	log.Printf("DB_DSN:%v", dsn)
+	dmodel.InitDatabase(dsn)
+
+	// 测试
+	var stocks []dmodel.Stock
+	stocks, err = dmodel.GetOwnerStocks("weibo_2685310785")
+	if err == nil {
+		for _, stock := range stocks {
+			bytes, _ := json.MarshalIndent(stock.GetTradeCfg(), "", "\t")
+			log.Printf("Code:%s,Name:%s,TradeCfg:%s", stock.Code, stock.Name, string(bytes))
+		}
+	}
+
 }
---
+++ model/init.go
@@ -0,0 +1,50 @@
+package domain
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// DB 数据库链接单例
+var DB *gorm.DB
+
+// Database 初始化
+func InitDatabase(connString string) {
+	// 初始化GORM日志
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Warn, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,       // Disable color
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(connString), &gorm.Config{
+		Logger: newLogger,
+	})
+	// Error
+	if connString == "" || err != nil {
+		log.Fatalf("dsn error: %v", err)
+		panic(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("init database error: %v", err)
+		panic(err)
+	}
+
+	//设置连接池
+	//空闲
+	sqlDB.SetMaxIdleConns(10)
+	//打开
+	sqlDB.SetMaxOpenConns(20)
+	DB = db
+
+}
